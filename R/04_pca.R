f08f1ec8da266d9b75ebf03815e0adf6a9fc9e43
nghiaagent <nghia6a1@gmail.com>
2026-01-30 00:35:06 +1000

    rename variables

--- R/04_pca.R
+++ R/04_pca.R
@@ -10,11 +10,13 @@ dge_voom <- readRDS(here::here("output/dge_voom.rds"))
 dge <- readRDS(here::here("output/dge.rds"))
 
 # Perform PCA
-plot_pca <- dge_voom$E %>%
+dge_pca <- dge_voom$E %>%
   pca(
     metadata = dge$samples,
     removeVar = 0.9
-  ) %>%
+  )
+
+plot_pca <- dge_pca %>%
   biplot(
     colby = "group",
     shape = "lane",
@@ -28,4 +30,5 @@ plot_pca <- dge_voom$E %>%
   )
 
 # Save data
+saveRDS(dge_pca, here::here("output/pca/dge_pca.rds"))
 saveRDS(plot_pca, here::here("output/pca/plot_pca.rds"))
